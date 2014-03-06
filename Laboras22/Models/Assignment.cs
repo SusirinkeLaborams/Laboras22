3a76b6011f069fb72afbaad05051c04aa1ee619f
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2014-03-06 12:30:53 +0200

    Add Models

---
+++ Laboras22/Models/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using Laboras22.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.Models
+{
+    class Assignment : IDataItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Laboras22/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using Laboras22.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.Models
+{
+    class Course : IDataItem
+    {
+        public int Id { get; set; }
+    }
+}
