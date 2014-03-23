db6e7c98422f96bade3cc42e9987e916627cbf06
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-03-23 15:42:37 +0200

    Login sequence diagram

---
+++ Laboras22/ValidationRules/UserNameValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Laboras22.ValidationRules
+{
+    class UserNameValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
