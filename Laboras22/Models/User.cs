7e5d7b846dccbdb37864c2a0a00d25d251bcd9e5
Sunius <suniusx@gmail.com>
2014-03-27 13:27:11 +0200

    Add user model.

---
+++ Laboras22/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using Laboras22.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.Models
+{
+    enum UserType
+    {
+        Student = 0,
+        Lecturer,
+        Administrator
+    }
+
+    class User : IDataItem
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
