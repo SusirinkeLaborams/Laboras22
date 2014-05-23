0dafdd621874e5cc01ada0d8e5570dfe262ea5c6
Sunius <suniusx@gmail.com>
2014-05-23 21:34:11 +0300

    Add styles.

--- Laboras22/Views/MainWindow.xaml.cs
+++ Laboras22/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Laboras22.Classes;
 using Laboras22.Interfaces;
+using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace Laboras22.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
