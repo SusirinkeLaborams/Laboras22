0b38a9cae221449950547236768a42f161edff1d
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-05-24 00:53:23 +0300

    Login window, viewmodel

---
+++ Laboras22/ViewModels/Users/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.ViewModels.Users
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        private string m_UserName;
+
+        public string UserName
+        {
+            get
+            {
+                return m_UserName;
+            }
+            set
+            {
+                m_UserName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public void Login(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
