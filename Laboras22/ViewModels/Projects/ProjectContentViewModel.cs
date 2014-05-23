c1f45a672cc4c772cf26b02a09b19569cfd4db9f
DariusL <d.lapunas@gmail.com>
2014-05-23 23:30:07 +0300

    MOAR view models

---
+++ Laboras22/ViewModels/Projects/ProjectContentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Laboras22.Models.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.ViewModels.Projects
+{
+    class ProjectContentViewModel : ViewModelBase<ProjectContent, ProjectContentViewModel>
+    {
+        public int Project { get { return model.Project; } set { model.Project = value; } }
+        public string URL { get { return model.URL; } set { model.URL = value; } }
+
+        protected override async Task RefreshFields()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Laboras22/ViewModels/Projects/ProjectViewModel.cs
+++ Laboras22/ViewModels/Projects/ProjectViewModel.cs
@@ -10,8 +10,8 @@ namespace Laboras22.ViewModels.Projects
     public class ProjectViewModel : ViewModelBase<Project, ProjectViewModel>
     {
         public string Name { get { return model.Name; } set { model.Name = value; } }
-        public int AssignmentId { get { return model.Assignment; } set { model.Assignment = value; } }
-        public int OwnerId { get { return model.Owner; } set { model.Owner = value; } }
+        public int Assignment { get { return model.Assignment; } set { model.Assignment = value; } }
+        public int Owner { get { return model.Owner; } set { model.Owner = value; } }
 
         protected override async Task RefreshFields()
         {
