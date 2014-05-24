4092b27be7c753b17e7d2d9d3303129fbd187553
DariusL <d.lapunas@gmail.com>
2014-05-24 18:57:47 +0300

    ParticipatedProjectListViewModel

---
+++ Laboras22/ViewModels/Projects/ParticipatedProjectListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.ViewModels.Projects
+{
+    class ParticipatedProjectListViewModel
+    {
+        public IEnumerable<ProjectViewModel> Projects { get; private set; }
+        private ParticipatedProjectListViewModel(IEnumerable<ProjectViewModel> projects)
+        {
+            Projects = projects;
+        }
+        public static async Task<ParticipatedProjectListViewModel> Create(int studentId)
+        {
+            var tmp = await ProjectParticipantViewModel.Where(x => x.StudentId == studentId);
+            var projects = await ProjectViewModel.Where(p => tmp.Any(a => a.Project.Id == p.Id));
+            return new ParticipatedProjectListViewModel(projects);
+        }
+    }
+}
--- Laboras22/ViewModels/Projects/ProjectParticipantViewModel.cs
+++ Laboras22/ViewModels/Projects/ProjectParticipantViewModel.cs
@@ -11,9 +11,11 @@ namespace Laboras22.ViewModels.Projects
     class ProjectParticipantViewModel : ViewModelBase<ProjectParticipant, ProjectParticipantViewModel>
     {
         public StudentViewModel Student { get; private set; }
+        public ProjectViewModel Project { get; private set; }
         protected override async Task RefreshFields()
         {
             Student = await StudentViewModel.Get(model.StudentId);
+            Project = await ProjectViewModel.Get(model.ProjectId);
         }
     }
 }
