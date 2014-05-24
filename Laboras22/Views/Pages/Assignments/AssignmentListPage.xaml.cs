bfd8ff1cb275f83e2cee2e744b9c5341b2d4fd9d
Sunius <suniusx@gmail.com>
2014-05-24 22:43:41 +0300

    Added AssignmentListPage.

---
+++ Laboras22/Views/Pages/Assignments/AssignmentListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras22.Views.Pages.Assignments
+{
+    /// <summary>
+    /// Interaction logic for AssignmentListPage.xaml
+    /// </summary>
+    public partial class AssignmentListPage : PageBase
+    {
+        public AssignmentListPage(MainWindow parentWindow) :
+            base(parentWindow)
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Laboras22/Views/Pages/Assignments/AssignmentPage.xaml.cs
+++ Laboras22/Views/Pages/Assignments/AssignmentPage.xaml.cs
@@ -3,20 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Laboras22.Views.Pages.Assignments
 {
-    public partial class AssignmentPage : Page
+    public partial class AssignmentPage : PageBase
     {
-        public AssignmentPage()
+        public AssignmentPage(MainWindow window) :
+            base(window)
         {
             InitializeComponent();
         }
