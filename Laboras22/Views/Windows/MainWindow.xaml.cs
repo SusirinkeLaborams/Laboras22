f48e39648d3d50a98a59375e6724f9278cbf3b29
DariusL <d.lapunas@gmail.com>
2014-05-24 21:04:02 +0300

    Added page stack to window, PageBase

--- Laboras22/Views/Windows/MainWindow.xaml.cs
+++ Laboras22/Views/Windows/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Laboras22.Classes;
+using Laboras22.Views.Pages;
 using MahApps.Metro.Controls;
+using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace Laboras22.Views
 {
@@ -8,12 +11,23 @@ namespace Laboras22.Views
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private Stack<Page> pages = new Stack<Page>();
         public MainWindow()
         {
             InitializeComponent();
             AzureService.Connect();
             
             m_Frame.Content = new LoginPage();
+        }
+        public void PushPage(Page page)
+        {
+            pages.Push(page);
+            m_Frame.Content = pages.Peek();
+        }
+        public void PopPage()
+        {
+            pages.Pop();
+            m_Frame.Content = pages.Count != 0 ? pages.Peek() : null;
         }
     }
 }
