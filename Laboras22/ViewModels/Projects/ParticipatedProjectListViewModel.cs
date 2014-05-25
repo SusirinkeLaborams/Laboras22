703a16d1515ecb16cf1eaca27b2583a2208a3fee
DariusL <d.lapunas@gmail.com>
2014-05-25 18:59:47 +0300

    Added all projects list

--- Laboras22/ViewModels/Projects/ParticipatedProjectListViewModel.cs
+++ Laboras22/ViewModels/Projects/ParticipatedProjectListViewModel.cs
@@ -8,15 +8,14 @@ namespace Laboras22.ViewModels.Projects
 {
     class ParticipatedProjectListViewModel : ProjectListViewModel
     {
-        protected ParticipatedProjectListViewModel(IEnumerable<ProjectViewModel> projects) : base(projects)
+        public ParticipatedProjectListViewModel(int sId)
         {
+            LoadProjects(sId);
         }
-        public static async Task<ParticipatedProjectListViewModel> Create(int studentId)
+        public async void LoadProjects(int studentId)
         {
             var tmp = await ProjectParticipantViewModel.Where(x => x.StudentId == studentId);
-            var projects = tmp.Select(a => a.Project);
-            projects = projects.Union(new List<ProjectViewModel>() { await ProjectViewModel.Create()});
-            return new ParticipatedProjectListViewModel(projects);
+            Projects = tmp.Select(a => a.Project);
         }
     }
 }
--- Laboras22/Views/Pages/Projects/ProjectList.xaml.cs
+++ Laboras22/Views/Pages/Projects/ProjectList.xaml.cs
@@ -22,16 +22,10 @@ namespace Laboras22.Views.Pages.Projects
     public partial class ProjectList : PageBase
     {
         private ProjectListViewModel viewModel;
-        private int userId;
-        public ProjectList(MainWindow window, int userId) : base(window)
+        internal ProjectList(MainWindow window, ProjectListViewModel viewModel) : base(window)
         {
-            this.userId = userId;
             InitializeComponent();
-        }
-        protected override async void OnInitialized(EventArgs e)
-        {
-            base.OnInitialized(e);
-            viewModel = await ParticipatedProjectListViewModel.Create(userId);
+            this.viewModel = viewModel;
             root.DataContext = viewModel;
         }
         private void ItemClick(object sender, RoutedEventArgs e)
