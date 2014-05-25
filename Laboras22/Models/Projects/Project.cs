dc5ade734e570535900ee784b8370d99db61e01b
Sunius <suniusx@gmail.com>
2014-05-25 14:10:51 +0300

    Add IsPublic property to Project model.

--- Laboras22/Models/Projects/Project.cs
+++ Laboras22/Models/Projects/Project.cs
@@ -10,8 +10,9 @@ namespace Laboras22.Models.Projects
     public class Project : IDataItem
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string ProjectName { get; set; }
         public int AssignmentId { get; set; }
         public int OwnerId { get; set; }
+        public bool IsPublic { get; set; }
     }
 }
